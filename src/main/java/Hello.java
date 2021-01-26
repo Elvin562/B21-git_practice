c4a93b19f694d1acbbe55ad3b7e932ec39a75fc0
Elvin562 <elvinbalayev86@gmail.com>
2021-01-26 15:37:57 -0500

    movie 2 branch watching on the phone 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
         System.out.println("Hello world,,,");
         System.out.println("B21 future SDET learning Git");
         System.out.println("More action here" );
+        //movie 2 on  the phone 10 minutes
     }
 
 }
