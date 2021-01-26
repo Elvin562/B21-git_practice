470609b85f8b32e1282e2fc62bd0ec729d3f8254
Elvin562 <elvinbalayev86@gmail.com>
2021-01-26 15:30:09 -0500

    movie 1 branch final work is done

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world,B22");
+        System.out.println("Hello world,,,");
         System.out.println("B21 future SDET learning Git");
-        //ready to go back to living room
-        //watching more movie
+        System.out.println("More action here" );
     }
 
 }
